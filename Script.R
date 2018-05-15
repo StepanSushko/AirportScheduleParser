5eaedf06a868c4f1822a39882e7392f845abf17e
Stepan <stepa_s85@mail.ru>
2018-05-15 23:16:37 +0300

    Mosaic plot added, read/write data.frame

--- Script.R
+++ Script.R
@@ -13,6 +13,8 @@ if (!require("rvest")) { install.packages("rvest"); require("rvest") }
 
 # Parsing -----
 
+dataDir = "C:/Users/stepa/OneDrive/DataScience/Airports/ЯндексТаблоПарсер/Platov"
+
 Sys.getlocale()
 LANG = "ru_RU.65001"
 LANG <- Sys.getenv("LANG")
@@ -93,11 +95,16 @@ OneWeek_TimeTable = function(station)
 
     A320_1 = "Airbus A320"
     A320_2 = "Airbus А320"
-
     df$`Самолёт`[df$`Самолёт` == A320_2] = A320_1
+
+    A321_1 = "Airbus А321"
+    A321_2 = "Airbus A321"
+    df$`Самолёт`[df$`Самолёт` == A321_2] = A321_1
 
     return(df)
 }
+
+#unique(df$`Самолёт`)
 
 
 "9600213" # Sheremetievo
@@ -107,7 +114,11 @@ OneWeek_TimeTable = function(station)
 "9600396" # Simpferopol
 station = "9600366" # Pulkovo
 
-df  = OneWeek_TimeTable("9866615")
+#df = OneWeek_TimeTable("9866615")
+#write.csv2(df, file.path(dataDir, "TimeTable.csv"))
+
+df = read.csv2( file.path(dataDir, "TimeTable.csv"))[,c(2:9)]
+
 #dff = df[, c(1,2,4,5)]
 df$`расписание`
 
@@ -116,7 +127,7 @@ df$`расписание`
 Freq_plot = function(dff, p_title)
 { 
     return(ggplot(data = dff) +
-        geom_bar(stat = "identity", aes(reorder(Var1, Freq), Freq), fill = "cyan3") +
+        geom_bar(stat = "identity", aes(reorder(Var1, Freq), Freq), fill = "cyan4") +
         geom_text(aes(reorder(Var1, Freq), label = Freq, y = Freq), size = 3, hjust = -0.1, colour = "dimgray", vjust = 0.25) +
         theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
         xlab("") + ylab("") + coord_flip() +
@@ -130,9 +141,9 @@ df_cities = as.data.frame(sort(table(df$`Направление`), decreasing = T))
 df_airlines = as.data.frame(sort(table(df$`Авиакомпания`), decreasing = T))
 df_jets = as.data.frame(sort(table(df$`Самолёт`), decreasing = T))
 
-Freq_plot(df_cities, "Рейсов из аэропорта Платов\nпо направлениям (в неделю)")
-Freq_plot(df_airlines, "Количество рейсов из аэропорта Платов\nпо авиакомпаниям (в неделю)")
-Freq_plot(df_jets, "Количество рейсов из аэропорта Платов\nпо типам самолётов (в неделю)")
+p1 = Freq_plot(df_cities, "Рейсов из аэропорта Платов\nпо направлениям (в неделю)")
+p2 = Freq_plot(df_airlines, "Количество рейсов из аэропорта Платов\nпо авиакомпаниям (в неделю)")
+p3 = Freq_plot(df_jets, "Количество рейсов из аэропорта Платов\nпо типам самолётов (в неделю)")
 
 
 # Frequncy by Day plot ---- 
@@ -143,7 +154,7 @@ df_wd_fly = data.frame(Var1 = as.character(names(tab2)), Freq = tab2)
 df_wd_fly$Var1 = as.integer(df_wd_fly$Var1)
 
 dff = df_wd_fly
-ggplot(data = dff) +
+p4 = ggplot(data = dff) +
         geom_bar(stat = "identity", aes(Var1, Freq), fill = "cyan3") +
         geom_text(aes(Var1, Freq, label = Freq, y = Freq), size = 3, colour = "dimgray", vjust = -0.3) +
         #        theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
@@ -178,42 +189,102 @@ sum(df_jets$`Ёмкость`) * 8 * 0.7
 
 
 
+
+library(grid)
+
+
+watermarkGrob <- function(lab = "PROOF ONLY") {
+    grob(lab = lab, cl = "watermark")
+}
+
+## custom draw method to
+## calculate expansion factor on-the-fly
+drawDetails.watermark <- function(x, rot = 45, ...) {
+    cex <- convertUnit(unit(1, "npc"), "mm", val = TRUE) 
+    convertUnit(unit(2, "grobwidth", textGrob(x$val)), "mm", val = TRUE)
+
+    grid.text(x$lab, rot = rot, gp = gpar(cex = cex, col = "black",
+                                        fontface = "bold", alpha = 0.5))
+
+}
+
+qplot(1:10, rnorm(10)) +
+    annotation_custom(xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf, watermarkGrob("bla"))
+
+
+#p3 +
+    qplot(1:10, rnorm(10)) +
+    annotation_custom(xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf, watermarkGrob())
+
+
+
+p4 +
+    annotate("text", x = Inf, y = -Inf,
+        label = "Степан Сушко для\nРостовский Городской Транспорт",
+        hjust = 1.1, vjust = -1.1, col = "white", cex = 4, fontface = "bold", alpha = 0.8)
+
+
+
+
+
+
 # Mosaic plot ----
+if (!require("vcd")) { install.packages("vcd"); require("vcd") }
+
 tab = table(df$`Авиакомпания`, df$`Направление`)
 
+#mosaic(df$`Авиакомпания` ~ df$`Направление`)
 
-mosaic(df$`Авиакомпания` ~ df$`Направление`)
-print(tbl)
-mosaic(tbl)
+tab = tab[as.character(df_airlines$Var1), as.character(df_cities$Var1)]
+#df_cross = as.data.frame.matrix(tab[1:5, 1:5])
 
+#mosaic(tab, legend = T, zero_size = 0, zero_split = T)
+#strucplot(tab[1:5, 1:5], legend = T, zero_size = 0, labeling = T)
 
-
-tab = tab[ as.character(df_airlines$Var1), as.character(df_cities$Var1)]
-df_cross = as.data.frame.matrix(tab)
-
-mosaic(tab, legend = T, zero_size = 0)
-
-mosaic(~`Направление` + `Авиакомпания`, data = df, zero_size = 0, zero_split = F, keep_aspect_ratio = F, ctx = 0.2)
-mosaic(~`Авиакомпания` + `Направление`, data = df, zero_size = 0, legend = T)
-
-# Plots
-if (!require("vcd")) { install.packages("vcd"); require("vcd") }
-mosaic(tab, legend = T)
-assoc(tab, legend = T, compress = T)
-
-
-res4
-
-
-
-library(MASS)
-data("Titanic")
-mosaic(Titanic)
-
-## Formula interface for tabulated data plus shading and legend:
-mosaic(~Sex + Age + Survived, data = Titanic,
-  main = "Survival on the Titanic", shade = TRUE, legend = TRUE)
+#mosaic(~`Направление` + `Авиакомпания`, data = df, zero_size = 0, zero_split = F, keep_aspect_ratio = F, ctx = 0.2)
+#mosaic(~`Авиакомпания` + `Направление`, data = df, zero_size = 0, legend = T)
 
 
 
 
+
+#
+par(mar = rep(.5, 4))
+mosaicplot( tab, las = 2, col = "cyan4", main = "")
+mosaicplot( t(tab), las = 2, col = "cyan4", main = "")
+
+
+
+tab2 = table(df$`Авиакомпания`, df$`Самолёт`)
+tab2 = tab2[as.character(df_airlines$Var1), as.character(df_jets$Var1)]
+
+par(mar = rep(.5, 4));
+mosaicplot(tab2, las = 2, col = "cyan4", main = "")
+mosaicplot( t(tab2), las = 2, col = "cyan4", main = "")
+
+
+
+
+
+
+
+
+# Bubble plot ----
+if (!require("RgoogleMaps")) { install.packages("RgoogleMaps"); require("RgoogleMaps") }
+
+map <- GetMap(center = c(df$lat[554], df$lon[554]), zoom = 15,
+       size = c(640, 640), destfile = file.path(tempdir(), "meuse.png"),
+        maptype = "mobile", SCALE = 1);
+
+par(cex = 2)
+
+
+
+#pdf( file = paste(plotDir, "/bus routes heat.pdf", sep = ""), width = 7, height = 7, pointsize = 12)
+png(filename = file.path(plotDir, "Bus routes heat.png"), width = 640, height = 640, units = "px", pointsize = 16, bg = "white", res = NA, family = "", restoreConsole = TRUE, type = c("cairo-png"))
+
+df3 = df[df$n_bus > 0,]
+bubbleMap(df3, coords = c("lon", "lat"), map,
+      zcol = 'n_bus', key.entries = 1 + 2 ^ (0:4), colPalette = colorRampPalette(c("steelblue", "tomato"))(length(1 + 2 ^ (0:4))), do.sqrt = T, alpha = 0.5, verbose = 0.5)
+
+dev.off()
